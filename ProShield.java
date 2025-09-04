27aea66db367d141f3de4de1e923c698385cff70
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 17:46:57 -0600

    Create ProShield.java

---
+++ ProShield.java
@@ -0,0 +1,74 @@
+package com.proshield;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.proshield.commands.ProShieldCommand;
+import com.proshield.managers.GUIManager;
+import com.proshield.managers.PlotManager;
+import com.proshield.managers.EconomyManager;
+import com.proshield.managers.BackupManager;
+import com.proshield.managers.DiscordManager;
+
+public class ProShield extends JavaPlugin {
+
+    private static ProShield instance;
+
+    private PlotManager plotManager;
+    private EconomyManager economyManager;
+    private BackupManager backupManager;
+    private DiscordManager discordManager;
+    private GUIManager guiManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig(); // generate config.yml if missing
+
+        // Initialize managers
+        plotManager = new PlotManager(this);
+        economyManager = new EconomyManager(this);
+        backupManager = new BackupManager(this);
+        discordManager = new DiscordManager(this);
+        guiManager = new GUIManager(this);
+
+        // Register commands
+        getCommand("proshield").setExecutor(new ProShieldCommand(this));
+
+        // Log startup
+        Bukkit.getLogger().info("[ProShield] Plugin enabled successfully!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Perform cleanup
+        backupManager.shutdown();
+        Bukkit.getLogger().info("[ProShield] Plugin disabled.");
+    }
+
+    public static ProShield getInstance() {
+        return instance;
+    }
+
+    // Getters for managers
+    public PlotManager getPlotManager() {
+        return plotManager;
+    }
+
+    public EconomyManager getEconomyManager() {
+        return economyManager;
+    }
+
+    public BackupManager getBackupManager() {
+        return backupManager;
+    }
+
+    public DiscordManager getDiscordManager() {
+        return discordManager;
+    }
+
+    public GUIManager getGuiManager() {
+        return guiManager;
+    }
+}

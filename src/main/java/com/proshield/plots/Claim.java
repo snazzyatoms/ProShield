510bf6f49f71594d95457c3df7b49ab0bb3796da
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 17:31:48 -0600

    Update Claim.java

--- src/main/java/com/proshield/plots/Claim.java
+++ src/main/java/com/proshield/plots/Claim.java
@@ -1,1 +1,29 @@
+package com.snazzyatoms.proshield.plots;
+
+/**
+ * Represents a simple claim (single point for now, expandable to areas later)
+ */
+public class Claim {
+    private final String world;
+    private final int x;
+    private final int z;
+
+    public Claim(String world, int x, int z) {
+        this.world = world;
+        this.x = x;
+        this.z = z;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getZ() {
+        return z;
+    }
+}
 

3418bfef5693d06b76b58ff557cde0e680a84372
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 13:39:06 -0600

    Update ProShieldCommand.java

--- src/main/java/com/proshield/commands/ProShieldCommand.java
+++ src/main/java/com/proshield/commands/ProShieldCommand.java
@@ -17,39 +17,17 @@ public class ProShieldCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.GOLD + "⚔ ProShield Commands:");
-            sender.sendMessage(ChatColor.YELLOW + "/proshield help " + ChatColor.GRAY + "- Show help menu");
-            sender.sendMessage(ChatColor.YELLOW + "/proshield info " + ChatColor.GRAY + "- Plugin info");
-            sender.sendMessage(ChatColor.YELLOW + "/proshield reload " + ChatColor.GRAY + "- Reload config");
+            sender.sendMessage(ChatColor.GREEN + "ProShield is running version " + plugin.getDescription().getVersion());
             return true;
         }
 
-        switch (args[0].toLowerCase()) {
-            case "reload":
-                if (sender.hasPermission("proshield.admin")) {
-                    plugin.reloadConfig();
-                    sender.sendMessage(ChatColor.GREEN + "✅ ProShield config reloaded.");
-                } else {
-                    sender.sendMessage(ChatColor.RED + "You don't have permission.");
-                }
-                return true;
-
-            case "info":
-                sender.sendMessage(ChatColor.GOLD + "🛡 ProShield " + plugin.getDescription().getVersion());
-                sender.sendMessage(ChatColor.GRAY + "Author: " + plugin.getDescription().getAuthors());
-                sender.sendMessage(ChatColor.GRAY + "Description: " + plugin.getDescription().getDescription());
-                return true;
-
-            case "help":
-                sender.sendMessage(ChatColor.GOLD + "⚔ ProShield Help Menu:");
-                sender.sendMessage(ChatColor.YELLOW + "/proshield help " + ChatColor.GRAY + "- Show this help menu");
-                sender.sendMessage(ChatColor.YELLOW + "/proshield info " + ChatColor.GRAY + "- Plugin info");
-                sender.sendMessage(ChatColor.YELLOW + "/proshield reload " + ChatColor.GRAY + "- Reload config");
-                return true;
-
-            default:
-                sender.sendMessage(ChatColor.RED + "Unknown command. Use /proshield help.");
-                return true;
+        if (args[0].equalsIgnoreCase("reload")) {
+            plugin.reloadConfig();
+            sender.sendMessage(ChatColor.YELLOW + "ProShield configuration reloaded!");
+            return true;
         }
+
+        sender.sendMessage(ChatColor.RED + "Unknown command. Try /proshield reload");
+        return false;
     }
 }

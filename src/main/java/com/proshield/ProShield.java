470fcdf1c11adaafc662bbf231b883d0cfb7b819
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 13:07:38 -0600

    Update ProShield.java

--- src/main/java/com/proshield/ProShield.java
+++ src/main/java/com/proshield/ProShield.java
@@ -1,29 +1,42 @@
 package com.snazzyatoms.proshield;
 
-import com.snazzyatoms.proshield.commands.ProShieldCommand;
 import com.snazzyatoms.proshield.listeners.AdminJoinListener;
 import com.snazzyatoms.proshield.listeners.PlotProtectionListener;
+import com.snazzyatoms.proshield.managers.PlotManager;
+import com.snazzyatoms.proshield.commands.ProShieldCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProShield extends JavaPlugin {
 
+    private static ProShield instance;
+    private PlotManager plotManager;
+
     @Override
     public void onEnable() {
-        getLogger().info("ProShield enabled!");
+        instance = this;
+        this.plotManager = new PlotManager();
 
         // Register commands
-        this.getCommand("proshield").setExecutor(new ProShieldCommand(this));
+        getCommand("proshield").setExecutor(new ProShieldCommand(this));
 
         // Register listeners
-        Bukkit.getPluginManager().registerEvents(new PlotProtectionListener(), this);
-        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new PlotProtectionListener(plotManager), this);
+        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(), this);
 
-        saveDefaultConfig(); // creates config folder + file if missing
+        getLogger().info("ProShield v" + getDescription().getVersion() + " has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("ProShield disabled!");
+        getLogger().info("ProShield has been disabled.");
+    }
+
+    public static ProShield getInstance() {
+        return instance;
+    }
+
+    public PlotManager getPlotManager() {
+        return plotManager;
     }
 }

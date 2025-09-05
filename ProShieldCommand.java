b1c14af7a0fe4ae72b19698019edf3ae86a8fe66
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 18:44:32 -0600

    Update ProShieldCommand.java

--- ProShieldCommand.java
+++ ProShieldCommand.java
@@ -1,10 +1,10 @@
-package com.proshield.commands;
+package com.proshield;
 
-import com.proshield.ProShield;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ProShieldCommand implements CommandExecutor {
 
@@ -16,32 +16,34 @@ public class ProShieldCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // No args → show help
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.YELLOW + "ProShield Commands:");
-            sender.sendMessage(ChatColor.AQUA + "/proshield reload" + ChatColor.GRAY + " - Reloads the plugin configuration");
-            sender.sendMessage(ChatColor.AQUA + "/proshield claim <radius>" + ChatColor.GRAY + " - Claim a plot");
-            sender.sendMessage(ChatColor.AQUA + "/proshield check" + ChatColor.GRAY + " - Check your plot info");
+            sender.sendMessage(ChatColor.YELLOW + "=== ProShield Commands ===");
+            sender.sendMessage(ChatColor.AQUA + "/proshield reload " + ChatColor.GRAY + " - Reloads the plugin configuration");
+            sender.sendMessage(ChatColor.AQUA + "/proshield claim <radius> " + ChatColor.GRAY + " - Claim a plot");
+            sender.sendMessage(ChatColor.AQUA + "/proshield check " + ChatColor.GRAY + " - Check your plot info");
             return true;
         }
 
+        // Handle `/proshield reload`
         if (args[0].equalsIgnoreCase("reload")) {
             if (!sender.hasPermission("proshield.reload")) {
-                sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+                sender.sendMessage(ChatColor.RED + "You don’t have permission to use this command.");
                 return true;
             }
 
+            // Reload config only (listeners stay registered)
             plugin.reloadConfig();
-            sender.sendMessage(ChatColor.GREEN + "ProShield configuration reloaded.");
+            plugin.saveConfig();
 
-            // Re-register listeners so they pick up the new config values
-            plugin.getServer().getPluginManager().registerEvents(
-                new com.proshield.listeners.PlotProtectionListener(plugin), plugin
-            );
+            sender.sendMessage(ChatColor.GREEN + "✅ ProShield configuration reloaded!");
+
+            // Log who executed it
+            String executor = (sender instanceof Player) ? sender.getName() : "CONSOLE";
+            plugin.getLogger().info("[ProShield] Configuration was reloaded by " + executor);
 
             return true;
         }
 
-        sender.sendMessage(ChatColor.RED + "Unknown command. Type /proshield for help.");
-        return true;
-    }
-}
+        // Unknown command
+        sender.sendMessage(ChatColor.RED + "Unknown command. Type /proshiel

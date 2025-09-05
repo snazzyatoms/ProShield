b027bb81c52abe7febaa4d4b086f9cc1e03cd9c1
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 14:59:10 -0600

    Update ProShield.java

--- src/main/java/com/proshield/ProShield.java
+++ src/main/java/com/proshield/ProShield.java
@@ -2,43 +2,41 @@ package com.snazzyatoms.proshield;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
 import com.snazzyatoms.proshield.commands.ProShieldCommand;
 import com.snazzyatoms.proshield.listeners.AdminJoinListener;
-import com.snazzyatoms.proshield.listeners.PlotProtectionListener;
-import com.snazzyatoms.proshield.managers.PlotManager;
+import com.snazzyatoms.proshield.listeners.CompassListener;
 
 public class ProShield extends JavaPlugin {
 
     private static ProShield instance;
-    private PlotManager plotManager;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        // Initialize PlotManager
-        plotManager = new PlotManager(this);
+        // Log startup
+        getLogger().info("ProShield v" + getDescription().getVersion() + " has been enabled!");
 
-        // Register commands
-        getCommand("proshield").setExecutor(new ProShieldCommand(this));
+        // Save default config if missing
+        saveDefaultConfig();
+
+        // Register command executor
+        if (getCommand("proshield") != null) {
+            getCommand("proshield").setExecutor(new ProShieldCommand(this));
+        }
 
         // Register listeners
-        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(this), this);
-        Bukkit.getPluginManager().registerEvents(new PlotProtectionListener(this), this);
-
-        getLogger().info("ProShield has been enabled!");
+        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(), this);
+        Bukkit.getPluginManager().registerEvents(new CompassListener(), this);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("ProShield has been disabled!");
+        getLogger().info("ProShield has been disabled.");
     }
 
     public static ProShield getInstance() {
         return instance;
-    }
-
-    public PlotManager getPlotManager() {
-        return plotManager;
     }
 }

3f7a2ca889fac98a2affb637e78857b88ef5cc2a
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 15:06:48 -0600

    Update ProShield.java

--- src/main/java/com/proshield/ProShield.java
+++ src/main/java/com/proshield/ProShield.java
@@ -1,42 +1,52 @@
 package com.snazzyatoms.proshield;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import com.snazzyatoms.proshield.commands.ProShieldCommand;
 import com.snazzyatoms.proshield.listeners.AdminJoinListener;
 import com.snazzyatoms.proshield.listeners.CompassListener;
+import com.snazzyatoms.proshield.listeners.GUIListener;
+import com.snazzyatoms.proshield.managers.PlotManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ProShield extends JavaPlugin {
 
     private static ProShield instance;
+    private PlotManager plotManager;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        // Log startup
-        getLogger().info("ProShield v" + getDescription().getVersion() + " has been enabled!");
-
-        // Save default config if missing
+        // Save default config.yml if missing
         saveDefaultConfig();
 
-        // Register command executor
-        if (getCommand("proshield") != null) {
-            getCommand("proshield").setExecutor(new ProShieldCommand(this));
-        }
+        // Initialize PlotManager
+        this.plotManager = new PlotManager(this);
 
-        // Register listeners
-        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(), this);
+        // Register commands
+        getCommand("proshield").setExecutor(new ProShieldCommand(this));
+
+        // Register event listeners
+        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(this), this);
         Bukkit.getPluginManager().registerEvents(new CompassListener(), this);
+        Bukkit.getPluginManager().registerEvents(new GUIListener(this), this);
+
+        getLogger().info(ChatColor.GREEN + "[ProShield] Enabled v" + getDescription().getVersion());
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("ProShield has been disabled.");
+        getLogger().info(ChatColor.RED + "[ProShield] Disabled.");
     }
 
+    // Access plugin instance
     public static ProShield getInstance() {
         return instance;
+    }
+
+    // Access PlotManager
+    public PlotManager getPlotManager() {
+        return plotManager;
     }
 }

6b516c219eca8ab39e164b75a4cd1d17601871c1
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 16:27:31 -0600

    Update ProShield.java

--- src/main/java/com/proshield/ProShield.java
+++ src/main/java/com/proshield/ProShield.java
@@ -1,16 +1,18 @@
 package com.snazzyatoms.proshield;
 
 import com.snazzyatoms.proshield.commands.ProShieldCommand;
-import com.snazzyatoms.proshield.listeners.CompassListener;
 import com.snazzyatoms.proshield.listeners.GUIListener;
+import com.snazzyatoms.proshield.listeners.PlotProtectionListener;
 import com.snazzyatoms.proshield.managers.GUIManager;
 import com.snazzyatoms.proshield.managers.PlotManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public class ProShield extends JavaPlugin {
 
     private static ProShield instance;
-
     private PlotManager plotManager;
     private GUIManager guiManager;
 
@@ -18,30 +20,33 @@ public class ProShield extends JavaPlugin {
     public void onEnable() {
         instance = this;
 
-        // Generate /plugins/ProShield/config.yml on first run
-        saveDefaultConfig();
-
-        // Managers
-        this.plotManager = new PlotManager(this);
-        this.guiManager  = new GUIManager();
-
-        // Commands
-        if (getCommand("proshield") != null) {
-            getCommand("proshield").setExecutor(new com.snazzyatoms.proshield.commands.ProShieldCommand(this));
-        } else {
-            getLogger().severe("Command 'proshield' not found. Check plugin.yml!");
+        // ✅ Ensure plugin folder exists
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdirs();
         }
 
-        // Listeners
-        getServer().getPluginManager().registerEvents(new CompassListener(guiManager), this);
-        getServer().getPluginManager().registerEvents(new GUIListener(plotManager), this);
+        // ✅ Save default config.yml if missing
+        saveDefaultConfig();
 
-        getLogger().info("[ProShield] Enabled v" + getDescription().getVersion());
+        // ✅ Initialize managers
+        plotManager = new PlotManager();
+        guiManager = new GUIManager(plotManager);
+
+        // ✅ Register listeners
+        Bukkit.getPluginManager().registerEvents(new PlotProtectionListener(plotManager), this);
+        Bukkit.getPluginManager().registerEvents(new GUIListener(guiManager), this);
+
+        // ✅ Register commands
+        if (getCommand("proshield") != null) {
+            getCommand("proshield").setExecutor(new ProShieldCommand(guiManager));
+        }
+
+        getLogger().info("✅ ProShield v" + getDescription().getVersion() + " has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("[ProShield] Disabled.");
+        getLogger().info("❌ ProShield has been disabled!");
     }
 
     public static ProShield getInstance() {

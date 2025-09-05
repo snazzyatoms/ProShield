4b13b621c4e4cbd3cf44b7bd6060014610ad4919
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 16:23:55 -0600

    Update PlotProtectionListener.java

--- src/main/java/com/proshield/listeners/PlotProtectionListener.java
+++ src/main/java/com/proshield/listeners/PlotProtectionListener.java
@@ -1,9 +1,6 @@
 package com.snazzyatoms.proshield.listeners;
 
-import com.snazzyatoms.proshield.ProShield;
 import com.snazzyatoms.proshield.managers.PlotManager;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -11,27 +8,25 @@ import org.bukkit.event.block.BlockPlaceEvent;
 
 public class PlotProtectionListener implements Listener {
 
-    private final ProShield plugin;
+    private final PlotManager plotManager;
 
-    public PlotProtectionListener(ProShield plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        if (!plugin.getPlotManager().canBuild(player, event.getBlock().getLocation())) {
-            event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You cannot break blocks here!");
-        }
+    public PlotProtectionListener(PlotManager plotManager) {
+        this.plotManager = plotManager;
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        Player player = event.getPlayer();
-        if (!plugin.getPlotManager().canBuild(player, event.getBlock().getLocation())) {
+        if (!plotManager.canBuild(event.getPlayer(), event.getBlock().getLocation())) {
             event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You cannot place blocks here!");
+            event.getPlayer().sendMessage("§cYou cannot build here!");
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!plotManager.canBuild(event.getPlayer(), event.getBlock().getLocation())) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage("§cYou cannot break blocks here!");
         }
     }
 }

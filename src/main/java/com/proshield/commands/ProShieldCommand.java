d042ac3e7e20bf54362e48051ec78c9e9e2b4484
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 23:18:18 -0600

    Update ProShieldCommand.java

--- src/main/java/com/proshield/commands/ProShieldCommand.java
+++ src/main/java/com/proshield/commands/ProShieldCommand.java
@@ -1,64 +1,49 @@
 package com.proshield.commands;
 
-import com.proshield.ProShield;
-import com.proshield.managers.PlotManager;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import com.proshield.managers.PlotManager;
 
 public class ProShieldCommand implements CommandExecutor {
 
-    private final ProShield plugin;
+    private final PlotManager plotManager;
 
-    public ProShieldCommand(ProShield plugin) {
-        this.plugin = plugin;
+    public ProShieldCommand(PlotManager plotManager) {
+        this.plotManager = plotManager;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("§cOnly players can use this command!");
+            sender.sendMessage("Only players can use this command.");
             return true;
         }
 
         Player player = (Player) sender;
+        Location location = player.getLocation();
 
         if (args.length == 0) {
-            player.sendMessage("§aProShield Commands:");
-            player.sendMessage("§e/proshield claim <radius> §7- Claim a plot at your location");
+            player.sendMessage(ChatColor.RED + "Usage: /proshield <add|remove>");
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("claim")) {
-            if (args.length < 2) {
-                player.sendMessage("§cUsage: /proshield claim <radius>");
-                return true;
-            }
-
-            try {
-                int radius = Integer.parseInt(args[1]);
-
-                if (radius < 5) {
-                    player.sendMessage("§cMinimum radius is 5!");
-                    return true;
-                }
-
-                PlotManager plotManager = plugin.getPlotManager();
-
-                if (plotManager.claimPlot(player, radius)) {
-                    player.sendMessage("§aPlot claimed successfully!");
-                } else {
-                    player.sendMessage("§cYou already own a plot!");
-                }
-
-            } catch (NumberFormatException e) {
-                player.sendMessage("§cRadius must be a number!");
-            }
+        if (args[0].equalsIgnoreCase("add")) {
+            plotManager.addProtectedPlot(location);
+            player.sendMessage(ChatColor.GREEN + "Plot protected at your location!");
             return true;
         }
 
-        player.sendMessage("§cUnknown command. Type /proshield for help.");
+        if (args[0].equalsIgnoreCase("remove")) {
+            plotManager.removeProtectedPlot(location);
+            player.sendMessage(ChatColor.YELLOW + "Plot unprotected at your location!");
+            return true;
+        }
+
+        player.sendMessage(ChatColor.RED + "Usage: /proshield <add|remove>");
         return true;
     }
 }

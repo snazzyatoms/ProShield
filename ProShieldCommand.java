36e2252132cb03a3468944e6c1b6ccdcfa9c8902
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 18:06:28 -0600

    Update ProShieldCommand.java

--- ProShieldCommand.java
+++ ProShieldCommand.java
@@ -1,8 +1,8 @@
 package com.proshield.commands;
 
 import com.proshield.ProShield;
-import com.proshield.managers.GUIManager;
-import org.bukkit.ChatColor;
+import com.proshield.managers.PlotManager;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,41 +18,67 @@ public class ProShieldCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
         if (!(sender instanceof Player)) {
-            sender.sendMessage(ChatColor.RED + "Only players can use ProShield commands!");
+            sender.sendMessage("§cOnly players can use this command!");
             return true;
         }
 
         Player player = (Player) sender;
+        PlotManager plotManager = plugin.getPlotManager();
 
         if (args.length == 0) {
-            // Open main GUI
-            GUIManager guiManager = plugin.getGuiManager();
-            guiManager.openPlayerGUI(player);
+            player.sendMessage("§aProShield Commands:");
+            player.sendMessage("§e/proshield claim <radius> §7- Claim a plot at your location");
+            player.sendMessage("§e/proshield info §7- View details of your claimed plot");
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("reload")) {
-            if (player.hasPermission("proshield.admin")) {
-                plugin.reloadConfig();
-                player.sendMessage(ChatColor.GREEN + "[ProShield] Config reloaded successfully.");
-            } else {
-                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+        if (args[0].equalsIgnoreCase("claim")) {
+            if (args.length < 2) {
+                player.sendMessage("§cUsage: /proshield claim <radius>");
+                return true;
+            }
+
+            try {
+                int radius = Integer.parseInt(args[1]);
+
+                if (radius < 5) {
+                    player.sendMessage("§cMinimum radius is 5!");
+                    return true;
+                }
+
+                if (plotManager.claimPlot(player, radius)) {
+                    player.sendMessage("§aPlot claimed successfully!");
+                } else {
+                    player.sendMessage("§cYou already own a plot!");
+                }
+
+            } catch (NumberFormatException e) {
+                player.sendMessage("§cRadius must be a number!");
             }
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("debug")) {
-            if (player.hasPermission("proshield.admin")) {
-                player.sendMessage(ChatColor.YELLOW + "[ProShield] Debug mode command is not implemented yet.");
-            } else {
-                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+        if (args[0].equalsIgnoreCase("info")) {
+            if (!plotManager.hasPlot(player)) {
+                player.sendMessage("§cYou don’t own a plot yet!");
+                return true;
             }
+
+            int radius = plotManager.getPlotRadius(player);
+            Location center = plotManager.getPlotCenter(player);
+
+            player.sendMessage("§aYour Plot Info:");
+            player.sendMessage("§7Center: §e" +
+                    center.getBlockX() + ", " +
+                    center.getBlockY() + ", " +
+                    center.getBlockZ() + " in world " + center.getWorld().getName());
+            player.sendMessage("§7Radius: §e" + radius + " blocks");
+
             return true;
         }
 
-        player.sendMessage(ChatColor.RED + "Unknown command. Use /proshield or /proshield reload");
+        player.sendMessage("§cUnknown command. Type /proshield for help.");
         return true;
     }
 }

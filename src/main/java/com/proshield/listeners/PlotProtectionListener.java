efb951b727ccb6619d05233940b96a27f6b286ca
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 23:17:46 -0600

    Update PlotProtectionListener.java

--- src/main/java/com/proshield/listeners/PlotProtectionListener.java
+++ src/main/java/com/proshield/listeners/PlotProtectionListener.java
@@ -1,46 +1,32 @@
 package com.proshield.listeners;
 
-import com.proshield.ProShield;
-import com.proshield.managers.PlotManager;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import com.proshield.managers.PlotManager;
 
 public class PlotProtectionListener implements Listener {
 
     private final PlotManager plotManager;
 
-    public PlotProtectionListener(ProShield plugin) {
-        this.plotManager = plugin.getPlotManager();
-    }
-
-    private boolean isInsideOwnPlot(Player player, Location loc) {
-        return plotManager.isInsidePlot(player.getUniqueId(), loc);
+    public PlotProtectionListener(PlotManager plotManager) {
+        this.plotManager = plotManager;
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        Location loc = event.getBlock().getLocation();
-
-        if (!isInsideOwnPlot(player, loc)) {
+        if (plotManager.isPlotProtected(event.getBlock().getLocation())) {
             event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You cannot break blocks here. This land is protected!");
+            event.getPlayer().sendMessage("This plot is protected!");
         }
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        Player player = event.getPlayer();
-        Location loc = event.getBlock().getLocation();
-
-        if (!isInsideOwnPlot(player, loc)) {
+        if (plotManager.isPlotProtected(event.getBlock().getLocation())) {
             event.setCancelled(true);
-            player.sendMessage(ChatColor.RED + "You cannot place blocks here. This land is protected!");
+            event.getPlayer().sendMessage("This plot is protected!");
         }
     }
 }

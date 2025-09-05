92d86da39dfe3354703c0edf81ab4df6c6fb2415
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 21:31:24 -0600

    Update PlotProtectionListener.java

--- PlotProtectionListener.java
+++ PlotProtectionListener.java
@@ -1,34 +1,39 @@
 package com.snazzyatoms.proshield;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.entity.Player;
 
 public class PlotProtectionListener implements Listener {
 
-    private final PlotManager plotManager;
+    private final ProShield plugin;
 
-    public PlotProtectionListener(PlotManager plotManager) {
-        this.plotManager = plotManager;
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        if (!plotManager.canBuild(player, event.getBlock().getLocation())) {
-            event.setCancelled(true);
-            player.sendMessage("§cYou cannot break blocks here!");
-        }
+    public PlotProtectionListener(ProShield plugin) {
+        this.plugin = plugin;
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        if (!plotManager.canBuild(player, event.getBlock().getLocation())) {
+        Location location = event.getBlockPlaced().getLocation();
+
+        if (!plugin.getPlotManager().canBuild(player, location)) {
             event.setCancelled(true);
-            player.sendMessage("§cYou cannot place blocks here!");
+            player.sendMessage("§cYou cannot build here! This area is protected.");
+        }
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        Player player = event.getPlayer();
+        Location location = event.getBlock().getLocation();
+
+        if (!plugin.getPlotManager().canBuild(player, location)) {
+            event.setCancelled(true);
+            player.sendMessage("§cYou cannot break blocks here! This area is protected.");
         }
     }
 }

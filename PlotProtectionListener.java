6f3dc38508c2f4b6a650b3b6045d4987fac82000
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 21:26:20 -0600

    Update PlotProtectionListener.java

--- PlotProtectionListener.java
+++ PlotProtectionListener.java
@@ -1,55 +1,34 @@
-package com.snazzyatoms.proshield.listeners;
+package com.snazzyatoms.proshield;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.entity.Player;
 
 public class PlotProtectionListener implements Listener {
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        // Protection logic here
-    }
+    private final PlotManager plotManager;
 
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        // Protection logic here
-    }
-}
-
-
-        // If not inside any plot, allow building
-        if (!plotManager.isInsideAnyPlot(event.getBlockPlaced().getLocation())) {
-            return;
-        }
-
-        // If inside their own plot, allow building
-        if (plotManager.isInsideOwnPlot(player, event.getBlockPlaced().getLocation())) {
-            return;
-        }
-
-        // Otherwise, cancel
-        event.setCancelled(true);
-        player.sendMessage(ChatColor.RED + "You cannot build in another player's plot!");
+    public PlotProtectionListener(PlotManager plotManager) {
+        this.plotManager = plotManager;
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
-
-        // If not inside any plot, allow breaking
-        if (!plotManager.isInsideAnyPlot(event.getBlock().getLocation())) {
-            return;
+        if (!plotManager.canBuild(player, event.getBlock().getLocation())) {
+            event.setCancelled(true);
+            player.sendMessage("§cYou cannot break blocks here!");
         }
+    }
 
-        // If inside their own plot, allow breaking
-        if (plotManager.isInsideOwnPlot(player, event.getBlock().getLocation())) {
-            return;
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        if (!plotManager.canBuild(player, event.getBlock().getLocation())) {
+            event.setCancelled(true);
+            player.sendMessage("§cYou cannot place blocks here!");
         }
-
-        // Otherwise, cancel
-        event.setCancelled(true);
-        player.sendMessage(ChatColor.RED + "You cannot break blocks in another player's plot!");
     }
 }

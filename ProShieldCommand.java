75edc50a1f8814c619ba7e31657ea06f9e4d37e9
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-04 18:16:36 -0600

    Update ProShieldCommand.java

--- ProShieldCommand.java
+++ ProShieldCommand.java
@@ -2,7 +2,7 @@ package com.proshield.commands;
 
 import com.proshield.ProShield;
 import com.proshield.managers.PlotManager;
-import org.bukkit.Location;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,74 +11,62 @@ import org.bukkit.entity.Player;
 public class ProShieldCommand implements CommandExecutor {
 
     private final ProShield plugin;
+    private final PlotManager plotManager;
 
     public ProShieldCommand(ProShield plugin) {
         this.plugin = plugin;
+        this.plotManager = plugin.getPlotManager();
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("§cOnly players can use this command!");
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
             return true;
         }
 
         Player player = (Player) sender;
-        PlotManager plotManager = plugin.getPlotManager();
 
         if (args.length == 0) {
-            player.sendMessage("§aProShield Commands:");
-            player.sendMessage("§e/proshield claim <radius> §7- Claim a plot at your location");
-            player.sendMessage("§e/proshield info §7- View details of your claimed plot");
+            player.sendMessage(ChatColor.YELLOW + "Usage: /proshield <claim|check>");
             return true;
         }
 
         if (args[0].equalsIgnoreCase("claim")) {
             if (args.length < 2) {
-                player.sendMessage("§cUsage: /proshield claim <radius>");
+                player.sendMessage(ChatColor.RED + "Usage: /proshield claim <radius>");
                 return true;
             }
 
+            int radius;
             try {
-                int radius = Integer.parseInt(args[1]);
-
-                if (radius < 5) {
-                    player.sendMessage("§cMinimum radius is 5!");
-                    return true;
-                }
-
-                if (plotManager.claimPlot(player, radius)) {
-                    player.sendMessage("§aPlot claimed successfully!");
-                } else {
-                    player.sendMessage("§cYou already own a plot!");
-                }
-
+                radius = Integer.parseInt(args[1]);
             } catch (NumberFormatException e) {
-                player.sendMessage("§cRadius must be a number!");
-            }
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("info")) {
-            if (!plotManager.hasPlot(player)) {
-                player.sendMessage("§cYou don’t own a plot yet!");
+                player.sendMessage(ChatColor.RED + "Radius must be a number!");
                 return true;
             }
 
-            int radius = plotManager.getPlotRadius(player);
-            Location center = plotManager.getPlotCenter(player);
+            boolean success = plotManager.claimPlot(player, radius);
+            if (success) {
+                player.sendMessage(ChatColor.GREEN + "You successfully claimed a plot with radius " + radius + "!");
+            } else {
+                player.sendMessage(ChatColor.RED + "You already have a plot!");
+            }
+            return true;
 
-            player.sendMessage("§aYour Plot Info:");
-            player.sendMessage("§7Center: §e" +
-                    center.getBlockX() + ", " +
-                    center.getBlockY() + ", " +
-                    center.getBlockZ() + " in world " + center.getWorld().getName());
-            player.sendMessage("§7Radius: §e" + radius + " blocks");
-
+        } else if (args[0].equalsIgnoreCase("check")) {
+            PlotManager.Plot plot = plotManager.getPlot(player);
+            if (plot == null) {
+                player.sendMessage(ChatColor.RED + "You don't have a plot yet!");
+            } else {
+                player.sendMessage(ChatColor.GREEN + "Your plot is centered at " +
+                        ChatColor.YELLOW + plot.getCenter().getBlockX() + ", " + plot.getCenter().getBlockZ() +
+                        ChatColor.GREEN + " with radius " + ChatColor.YELLOW + plot.getRadius());
+            }
             return true;
         }
 
-        player.sendMessage("§cUnknown command. Type /proshield for help.");
+        player.sendMessage(ChatColor.RED + "Unknown subcommand!");
         return true;
     }
 }

e2cc2872c598f657f3ece335826773ca9a39e034
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 18:55:18 -0600

    Update proshield.java

--- src/main/java/com/snazzyatoms/proshield/proshield.java
+++ src/main/java/com/snazzyatoms/proshield/proshield.java
@@ -1,40 +1,21 @@
 package com.snazzyatoms.proshield;
 
+import com.snazzyatoms.proshield.GUI.GUIListener;
+import com.snazzyatoms.proshield.GUI.GUIManager;
+import com.snazzyatoms.proshield.commands.ProShieldCommand;
+import com.snazzyatoms.proshield.listeners.AdminJoinListener;
+import com.snazzyatoms.proshield.listeners.PlotProtectionListener;
+import com.snazzyatoms.proshield.plots.PlotManager;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.snazzyatoms.proshield.commands.ProShieldCommand;
-import com.snazzyatoms.proshield.listeners.GUIListener;
-import com.snazzyatoms.proshield.GUI.GUIManager;
-import com.snazzyatoms.proshield.plots.PlotManager;
-
 public class ProShield extends JavaPlugin {
+
     private static ProShield instance;
+
     private PlotManager plotManager;
     private GUIManager guiManager;
-
-    @Override
-    public void onEnable() {
-        instance = this;
-
-        // Managers
-        this.plotManager = new PlotManager(this);
-        this.guiManager = new GUIManager(this, plotManager);
-
-        // Commands
-        getCommand("proshield").setExecutor(new ProShieldCommand(this, plotManager, guiManager));
-
-        // Listeners
-        Bukkit.getPluginManager().registerEvents(new GUIListener(guiManager), this);
-
-        // Config setup
-        saveDefaultConfig();
-    }
-
-    @Override
-    public void onDisable() {
-        // Cleanup if needed
-    }
 
     public static ProShield getInstance() {
         return instance;
@@ -47,5 +28,37 @@ public class ProShield extends JavaPlugin {
     public GUIManager getGuiManager() {
         return guiManager;
     }
-}
 
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // ensure data folder + config
+        saveDefaultConfig();
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        // managers
+        plotManager = new PlotManager(this);
+        guiManager = new GUIManager(this, plotManager);
+
+        // listeners
+        Bukkit.getPluginManager().registerEvents(new GUIListener(guiManager), this);
+        Bukkit.getPluginManager().registerEvents(new PlotProtectionListener(plotManager), this);
+        Bukkit.getPluginManager().registerEvents(new AdminJoinListener(this), this);
+
+        // command
+        getCommand("proshield").setExecutor(new ProShieldCommand(this));
+
+        getLogger().info("ProShield enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        // persist any in-memory updates to config
+        FileConfiguration cfg = getConfig();
+        plotManager.flushToConfig(cfg);
+        saveConfig();
+        getLogger().info("ProShield disabled.");
+    }
+}

2e4b029c48fe64928733e4ecc850c2b57b7d420b
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 22:18:40 -0600

    Update BlockProtectionListener.java

--- src/main/java/com/snazzyatoms/proshield/plots/BlockProtectionListener.java
+++ src/main/java/com/snazzyatoms/proshield/plots/BlockProtectionListener.java
@@ -1,3 +1,4 @@
+// path: src/main/java/com/snazzyatoms/proshield/plots/BlockProtectionListener.java
 package com.snazzyatoms.proshield.plots;
 
 import com.snazzyatoms.proshield.ProShield;
@@ -10,31 +11,33 @@ import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockProtectionListener implements Listener {
 
+    private final ProShield plugin;
     private final PlotManager plotManager;
 
     public BlockProtectionListener(ProShield plugin) {
+        this.plugin = plugin;
         this.plotManager = plugin.getPlotManager();
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
-        Location loc = event.getBlock().getLocation();
+        Location location = event.getBlock().getLocation();
 
-        if (!plotManager.canBuild(player, loc)) {
+        if (!plotManager.canModify(player, location)) {
             event.setCancelled(true);
-            player.sendMessage("§cThis chunk is protected!");
+            player.sendMessage("§cYou cannot break blocks here!");
         }
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
-        Location loc = event.getBlock().getLocation();
+        Location location = event.getBlock().getLocation();
 
-        if (!plotManager.canBuild(player, loc)) {
+        if (!plotManager.canModify(player, location)) {
             event.setCancelled(true);
-            player.sendMessage("§cThis chunk is protected!");
+            player.sendMessage("§cYou cannot place blocks here!");
         }
     }
 }

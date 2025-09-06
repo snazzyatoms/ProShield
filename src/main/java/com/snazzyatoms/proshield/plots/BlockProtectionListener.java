46fff7154f9e6557c29705cc68882c87534492c6
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 22:08:29 -0600

    Update BlockProtectionListener.java

--- src/main/java/com/snazzyatoms/proshield/plots/BlockProtectionListener.java
+++ src/main/java/com/snazzyatoms/proshield/plots/BlockProtectionListener.java
@@ -1,6 +1,8 @@
 package com.snazzyatoms.proshield.plots;
 
 import com.snazzyatoms.proshield.ProShield;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -8,31 +10,31 @@ import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockProtectionListener implements Listener {
 
-    private final ProShield plugin;
+    private final PlotManager plotManager;
 
     public BlockProtectionListener(ProShield plugin) {
-        this.plugin = plugin;
+        this.plotManager = plugin.getPlotManager();
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        // Example: Prevent breaking in protected areas
-        if (plugin.getPlotManager().isInClaim(event.getBlock().getLocation())) {
-            if (!plugin.getPlotManager().canBuild(event.getPlayer(), event.getBlock().getLocation())) {
-                event.setCancelled(true);
-                event.getPlayer().sendMessage("§cYou cannot break blocks in a protected claim!");
-            }
+        Player player = event.getPlayer();
+        Location loc = event.getBlock().getLocation();
+
+        if (!plotManager.canBuild(player, loc)) {
+            event.setCancelled(true);
+            player.sendMessage("§cThis chunk is protected!");
         }
     }
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-        // Example: Prevent placing in protected areas
-        if (plugin.getPlotManager().isInClaim(event.getBlockPlaced().getLocation())) {
-            if (!plugin.getPlotManager().canBuild(event.getPlayer(), event.getBlockPlaced().getLocation())) {
-                event.setCancelled(true);
-                event.getPlayer().sendMessage("§cYou cannot place blocks in a protected claim!");
-            }
+        Player player = event.getPlayer();
+        Location loc = event.getBlock().getLocation();
+
+        if (!plotManager.canBuild(player, loc)) {
+            event.setCancelled(true);
+            player.sendMessage("§cThis chunk is protected!");
         }
     }
 }

a5b3b58a3aad3e6cd6903142064777aebc8ef225
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 23:18:14 -0600

    Update ProShieldCommand.java

--- src/main/java/com/snazzyatoms/proshield/commands/ProShieldCommand.java
+++ src/main/java/com/snazzyatoms/proshield/commands/ProShieldCommand.java
@@ -1,6 +1,9 @@
-package com.snazzytom.proshield.commands;
+// path: src/main/java/com/snazzyatoms/proshield/commands/ProShieldCommand.java
+package com.snazzyatoms.proshield.commands;
 
-import com.snazzytom.proshield.plots.PlotManager;
+import com.snazzyatoms.proshield.ProShield;
+import com.snazzyatoms.proshield.plots.PlotManager;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,37 +12,48 @@ import org.bukkit.entity.Player;
 
 public class ProShieldCommand implements CommandExecutor {
 
+    private final ProShield plugin;
     private final PlotManager plotManager;
 
-    public ProShieldCommand(PlotManager plotManager) {
+    public ProShieldCommand(ProShield plugin, PlotManager plotManager) {
+        this.plugin = plugin;
         this.plotManager = plotManager;
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players can use this command.");
+        if (!(sender instanceof Player player)) {
+            sender.sendMessage(ChatColor.RED + "Only players can use this command!");
             return true;
         }
-
-        Player player = (Player) sender;
-        Location location = player.getLocation();
 
         if (args.length == 0) {
-            player.sendMessage("Usage: /proshield <create|remove>");
+            sender.sendMessage(ChatColor.YELLOW + "Usage: /proshield <claim|unclaim>");
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("create")) {
-            plotManager.createClaim(player, location); // ✅ fixed
-            player.sendMessage("Plot created at your location!");
-        } else if (args[0].equalsIgnoreCase("remove")) {
-            plotManager.removeClaim(player, location); // ✅ fixed
-            player.sendMessage("Plot removed at your location!");
-        } else {
-            player.sendMessage("Usage: /proshield <create|remove>");
-        }
+        Location loc = player.getLocation();
 
-        return true;
+        switch (args[0].toLowerCase()) {
+            case "claim":
+                if (plotManager.createClaim(player.getUniqueId(), loc)) {
+                    player.sendMessage(ChatColor.GREEN + "Chunk claimed successfully!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "This chunk is already claimed.");
+                }
+                return true;
+
+            case "unclaim":
+                if (plotManager.removeClaim(player.getUniqueId(), loc)) {
+                    player.sendMessage(ChatColor.GREEN + "Chunk unclaimed successfully!");
+                } else {
+                    player.sendMessage(ChatColor.RED + "You do not own this claim.");
+                }
+                return true;
+
+            default:
+                player.sendMessage(ChatColor.YELLOW + "Usage: /proshield <claim|unclaim>");
+                return true;
+        }
     }
 }

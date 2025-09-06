3eaac657dcc0b94b5e27197b696fda72fec6e9ba
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-05 21:39:37 -0600

    Update PlayerJoinListener.java

--- src/main/java/com/snazzyatoms/proshield/plots/PlayerJoinListener.java
+++ src/main/java/com/snazzyatoms/proshield/plots/PlayerJoinListener.java
@@ -1,20 +1,24 @@
-// path: src/main/java/com/snazzyatoms/proshield/plots/PlayerJoinListener.java
 package com.snazzyatoms.proshield.plots;
 
-import com.snazzyatoms.proshield.gui.GUIManager;
+import com.snazzyatoms.proshield.ProShield;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoinListener implements Listener {
 
+    private final ProShield plugin;
+
+    public PlayerJoinListener(ProShield plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        // Auto-give compass to ops (or anyone with permission)
-        if (e.getPlayer().isOp() || e.getPlayer().hasPermission("proshield.compass")) {
-            if (!e.getPlayer().getInventory().containsAtLeast(GUIManager.createAdminCompass(), 1)) {
-                e.getPlayer().getInventory().addItem(GUIManager.createAdminCompass());
-            }
-        }
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Example usage of plugin instance
+        plugin.getLogger().info(event.getPlayer().getName() + " joined the server!");
+
+        // You can also hook into PlotManager here if needed
+        // plugin.getPlotManager().loadClaims(event.getPlayer());
     }
 }

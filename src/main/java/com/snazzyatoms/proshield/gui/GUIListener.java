faa3baf6eb5634b5508653810403bb8fb8ba00eb
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-06 00:24:15 -0600

    Update GUIListener.java

--- src/main/java/com/snazzyatoms/proshield/gui/GUIListener.java
+++ src/main/java/com/snazzyatoms/proshield/gui/GUIListener.java
@@ -1,13 +1,14 @@
-// path: src/main/java/com/snazzyatoms/proshield/gui/GUIListener.java
 package com.snazzyatoms.proshield.gui;
 
 import com.snazzyatoms.proshield.plots.PlotManager;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class GUIListener implements Listener {
 
@@ -19,32 +20,38 @@ public class GUIListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (!(event.getWhoClicked() instanceof Player player)) return;
+        if (!(event.getWhoClicked() instanceof Player)) return;
 
-        String title = event.getView().getTitle();
-        if (!title.equalsIgnoreCase("ProShield GUI")) return;
+        Player player = (Player) event.getWhoClicked();
+        ItemStack clicked = event.getCurrentItem();
+
+        if (clicked == null || !event.getView().getTitle().equals(ChatColor.DARK_GREEN + "ProShield Menu")) {
+            return;
+        }
 
         event.setCancelled(true);
         Location loc = player.getLocation();
 
-        switch (event.getCurrentItem().getType()) {
-            case GRASS_BLOCK -> {
+        switch (clicked.getType()) {
+            case GRASS_BLOCK:
                 if (plotManager.createClaim(player.getUniqueId(), loc)) {
                     player.sendMessage(ChatColor.GREEN + "You claimed this chunk!");
                 } else {
                     player.sendMessage(ChatColor.RED + "This chunk is already claimed.");
                 }
-            }
-            case BARRIER -> {
+                break;
+
+            case BARRIER:
                 if (plotManager.removeClaim(player.getUniqueId(), loc)) {
                     player.sendMessage(ChatColor.GREEN + "You unclaimed this chunk!");
                 } else {
-                    player.sendMessage(ChatColor.RED + "You don’t own this claim.");
+                    player.sendMessage(ChatColor.RED + "You don't own this claim.");
                 }
-            }
-            default -> {
-                // ignore other clicks
-            }
+                break;
+
+            default:
+                // Ignore other clicks
+                break;
         }
     }
 }

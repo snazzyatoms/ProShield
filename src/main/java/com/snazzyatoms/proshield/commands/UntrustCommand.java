bd3b0b8ae6a9373ad9ce74eb0e2562ca1a8774d7
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-10 17:05:01 -0600

    Update UntrustCommand.java

--- src/main/java/com/snazzyatoms/proshield/commands/UntrustCommand.java
+++ src/main/java/com/snazzyatoms/proshield/commands/UntrustCommand.java
@@ -5,55 +5,75 @@ import com.snazzyatoms.proshield.plots.Plot;
 import com.snazzyatoms.proshield.plots.PlotManager;
 import com.snazzyatoms.proshield.roles.ClaimRoleManager;
 import com.snazzyatoms.proshield.util.MessagesUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
+/**
+ * /untrust <player>
+ *
+ * Removes a player's trust from your current claim.
+ */
 public class UntrustCommand implements CommandExecutor {
 
     private final ProShield plugin;
-    private final PlotManager plotManager;
-    private final ClaimRoleManager roleManager;
-    private final MessagesUtil messages;
+    private final PlotManager plots;
+    private final ClaimRoleManager roles;
+    private final MessagesUtil msg;
 
-    public UntrustCommand(ProShield plugin, PlotManager plotManager, ClaimRoleManager roleManager) {
+    public UntrustCommand(ProShield plugin, PlotManager plots, ClaimRoleManager roles) {
         this.plugin = plugin;
-        this.plotManager = plotManager;
-        this.roleManager = roleManager;
-        this.messages = plugin.getMessagesUtil();
+        this.plots = plots;
+        this.roles = roles;
+        this.msg = plugin.getMessagesUtil();
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player player)) {
-            messages.send(sender, "general.no-console");
-            return true;
-        }
-
-        Plot plot = plotManager.getPlot(player.getLocation().getChunk());
-        if (plot == null) {
-            messages.send(player, "untrust.no-claim");
-            return true;
-        }
-
-        if (!plot.isOwner(player.getUniqueId())) {
-            messages.send(player, "untrust.not-owner");
+            msg.send(sender, "error.player-only");
             return true;
         }
 
         if (args.length < 1) {
-            messages.send(player, "untrust.usage");
+            msg.send(player, "untrust.usage");
             return true;
         }
 
-        String targetName = args[0];
-        if (!plot.removeTrusted(targetName)) {
-            messages.send(player, "untrust.not-trusted", targetName);
+        // Resolve target UUID
+        OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+        UUID targetId = target.getUniqueId();
+        if (targetId.equals(player.getUniqueId())) {
+            msg.send(player, "untrust.cannot-self");
             return true;
         }
 
-        messages.send(player, "untrust.success", targetName);
+        // Ensure player is standing in a claim
+        Plot plot = plots.getPlot(player.getLocation());
+        if (plot == null) {
+            msg.send(player, "untrust.no-claim");
+            return true;
+        }
+
+        // Must be owner
+        if (!plot.isOwner(player.getUniqueId())) {
+            msg.send(player, "untrust.not-owner", plot.getDisplayNameSafe());
+            return true;
+        }
+
+        // Remove trust
+        boolean removed = roles.untrustPlayer(plot, targetId);
+        if (!removed) {
+            msg.send(player, "untrust.not-trusted", target.getName());
+            return true;
+        }
+
+        msg.send(player, "untrust.success", target.getName());
         return true;
     }
 }

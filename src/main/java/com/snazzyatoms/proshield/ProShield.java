457f21848ee788d5bf49f49f1feec65371b35696
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-12 15:48:26 -0600

    Update ProShield.java

--- src/main/java/com/snazzyatoms/proshield/ProShield.java
+++ src/main/java/com/snazzyatoms/proshield/ProShield.java
@@ -7,7 +7,8 @@ import com.snazzyatoms.proshield.compass.CompassManager;
 import com.snazzyatoms.proshield.gui.GUIListener;
 import com.snazzyatoms.proshield.gui.GUIManager;
 import com.snazzyatoms.proshield.gui.cache.GUICache;
-import com.snazzyatoms.proshield.plots.PlotListener;
+import com.snazzyatoms.proshield.plots.EntityBorderRepelTask;
+import com.snazzyatoms.proshield.plots.EntityMobRepelTask;
 import com.snazzyatoms.proshield.plots.PlotManager;
 import com.snazzyatoms.proshield.roles.ClaimRoleManager;
 import com.snazzyatoms.proshield.util.MessagesUtil;
@@ -55,6 +56,9 @@ public class ProShield extends JavaPlugin {
         // Listeners
         registerListeners();
 
+        // Background tasks (repel)
+        scheduleRepelTasks();
+
         getLogger().info("ProShield v" + getDescription().getVersion() + " enabled.");
     }
 
@@ -67,73 +71,61 @@ public class ProShield extends JavaPlugin {
     }
 
     private void registerCommands() {
-        // Root /proshield command
         if (getCommand("proshield") != null) {
             getCommand("proshield").setExecutor(
                 new ProShieldCommand(this, plotManager, guiManager, compassManager)
             );
+            getCommand("proshield").setTabCompleter(
+                new ProShieldCommand(this, plotManager, guiManager, compassManager)
+            );
         }
 
-        // Player dispatcher for claim-related commands
         PlayerCommandDispatcher playerCommandDispatcher =
             new PlayerCommandDispatcher(this, plotManager, roleManager, messages);
 
         if (getCommand("claim") != null) getCommand("claim").setExecutor(playerCommandDispatcher);
         if (getCommand("unclaim") != null) getCommand("unclaim").setExecutor(playerCommandDispatcher);
-        if (getCommand("trust") != null) getCommand("trust").setExecutor(playerCommandDispatcher);
-        if (getCommand("untrust") != null) getCommand("untrust").setExecutor(playerCommandDispatcher);
+        if (getCommand("trust") != null) {
+            getCommand("trust").setExecutor(playerCommandDispatcher);
+            getCommand("trust").setTabCompleter(playerCommandDispatcher);
+        }
+        if (getCommand("untrust") != null) {
+            getCommand("untrust").setExecutor(playerCommandDispatcher);
+            getCommand("untrust").setTabCompleter(playerCommandDispatcher);
+        }
         if (getCommand("roles") != null) getCommand("roles").setExecutor(playerCommandDispatcher);
         if (getCommand("transfer") != null) getCommand("transfer").setExecutor(playerCommandDispatcher);
     }
 
     private void registerListeners() {
-        // GUI listener
         Bukkit.getPluginManager().registerEvents(new GUIListener(this, guiManager), this);
+    }
 
-        // Unified plot protections & claim messages
-        Bukkit.getPluginManager().registerEvents(
-            new PlotListener(this, plotManager, roleManager, messages), this
-        );
+    private void scheduleRepelTasks() {
+        boolean borderEnabled = getConfig().getBoolean("protection.mobs.border-repel.enabled", true);
+        boolean despawnInside = getConfig().getBoolean("protection.mobs.despawn-inside", true);
+
+        if (borderEnabled) {
+            new EntityBorderRepelTask(this, plotManager).runTaskTimer(this, 20L, 20L * 3); // every 3s
+        }
+        if (despawnInside) {
+            new EntityMobRepelTask(this, plotManager).runTaskTimer(this, 20L, 20L * 5); // every 5s
+        }
     }
 
     // Getters
-    public MessagesUtil getMessagesUtil() {
-        return messages;
-    }
-
-    public PlotManager getPlotManager() {
-        return plotManager;
-    }
-
-    public ClaimRoleManager getRoleManager() {
-        return roleManager;
-    }
-
-    public GUICache getGuiCache() {
-        return guiCache;
-    }
-
-    public GUIManager getGuiManager() {
-        return guiManager;
-    }
-
-    public CompassManager getCompassManager() {
-        return compassManager;
-    }
+    public MessagesUtil getMessagesUtil() { return messages; }
+    public PlotManager getPlotManager() { return plotManager; }
+    public ClaimRoleManager getRoleManager() { return roleManager; }
+    public GUICache getGuiCache() { return guiCache; }
+    public GUIManager getGuiManager() { return guiManager; }
+    public CompassManager getCompassManager() { return compassManager; }
 
     // Debug support
-    public boolean isDebugEnabled() {
-        return debugEnabled;
-    }
-
-    public void setDebugEnabled(boolean enabled) {
-        this.debugEnabled = enabled;
-    }
-
+    public boolean isDebugEnabled() { return debugEnabled; }
+    public void setDebugEnabled(boolean enabled) { this.debugEnabled = enabled; }
     public void toggleDebug() {
         this.debugEnabled = !this.debugEnabled;
-        if (messages != null) {
-            messages.debug("Debug mode toggled: " + (this.debugEnabled ? "ON" : "OFF"));
-        }
+        if (messages != null) messages.debug("Debug mode toggled: " + (this.debugEnabled ? "ON" : "OFF"));
     }
 }

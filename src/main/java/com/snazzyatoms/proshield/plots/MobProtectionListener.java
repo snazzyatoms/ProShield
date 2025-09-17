558c34938e9e0b2c135b5e44f9e6145e8994dfdf
snazzyatoms <142071673+snazzyatoms@users.noreply.github.com>
2025-09-16 20:15:13 -0600

    Update MobProtectionListener.java

--- src/main/java/com/snazzyatoms/proshield/plots/MobProtectionListener.java
+++ src/main/java/com/snazzyatoms/proshield/plots/MobProtectionListener.java
@@ -1,9 +1,11 @@
 package com.snazzyatoms.proshield.plots;
 
 import com.snazzyatoms.proshield.ProShield;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Monster;
+import com.snazzyatoms.proshield.util.MessagesUtil;
+import org.bukkit.Location;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
@@ -12,30 +14,54 @@ public class MobProtectionListener implements Listener {
 
     private final ProShield plugin;
     private final PlotManager plotManager;
+    private final MessagesUtil messages;
 
     public MobProtectionListener(ProShield plugin, PlotManager plotManager) {
         this.plugin = plugin;
         this.plotManager = plotManager;
+        this.messages = plugin.getMessagesUtil();
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onMobSpawn(CreatureSpawnEvent event) {
-        Plot plot = plotManager.getPlotAt(event.getLocation());
-        if (plot != null && !plot.getFlag("mob-spawn")) {
+        Location loc = event.getLocation();
+        Entity entity = event.getEntity();
+
+        // Optional: skip natural passive mob spawns
+        if (entity instanceof Animals || entity instanceof Ambient) return;
+
+        Plot plot = plotManager.getPlotAt(loc);
+        if (plot == null) return;
+
+        boolean allowed = plot.getFlag("mob-spawn");
+
+        if (!allowed) {
             event.setCancelled(true);
+            debug("Blocked spawn of " + entity.getType() + " at " + locToString(loc));
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onMobTarget(EntityTargetEvent event) {
         Entity target = event.getTarget();
-        if (target == null) return;
+        if (target == null || !(target instanceof Player)) return;
 
         Plot plot = plotManager.getPlotAt(target.getLocation());
-        if (plot != null && plot.getFlag("safezone")) {
-            if (event.getEntity() instanceof Monster) {
-                event.setCancelled(true);
-            }
+        if (plot == null) return;
+
+        if (plot.getFlag("safezone") && event.getEntity() instanceof Monster) {
+            event.setCancelled(true);
+            debug("Prevented mob targeting in safezone: " + event.getEntity().getType());
         }
+    }
+
+    private void debug(String msg) {
+        if (plugin.isDebugEnabled()) {
+            plugin.getLogger().info("[MobProtection] " + msg);
+        }
+    }
+
+    private String locToString(Location loc) {
+        return loc.getWorld().getName() + " (" + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + ")";
     }
 }
